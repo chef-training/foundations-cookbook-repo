04c9afcf79e266eb9cccc953813eb77a310461d6
steve <steve@example.com>
2019-07-03 16:25:41 +0000

    complete myiis cookbook

--- myiis/recipes/server.rb
+++ myiis/recipes/server.rb
@@ -4,17 +4,13 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 powershell_script 'Install IIS' do
-  code 'Add-WindowsFeature Web-Server'
+	code 'Add-WindowsFeature Web-Server'
 end
 
-file 'C:\inetpub\wwwroot\Default.htm' do
-  content "<h1>Hello, world!</h1>
-<h2>PLATFORM: #{node['platform']}</h2>
-<h2>HOSTNAME: #{node['hostname']}</h2>
-<h2>MEMORY:   #{node['memory']['total']}</h2>
-<h2>CPU Mhz:  #{node['cpu']['0']['mhz']}</h2>"
+  template 'c:\inetpub\wwwroot\Default.htm' do
+    source 'Default.htm.erb'
 end
 
 service 'w3svc' do
-  action [:enable, :start]
+	action [:enable, :start]
 end
